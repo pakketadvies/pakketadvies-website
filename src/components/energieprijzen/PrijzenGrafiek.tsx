d6759f935bfb38b68d3d6247f85286a04c86eade
Rick Schlimback <rickschlimback@iMac-van-Rick.local>
2025-12-01 15:49:31 +0100

    Fix chart container styling met expliciete waarden

--- src/components/energieprijzen/PrijzenGrafiek.tsx
+++ src/components/energieprijzen/PrijzenGrafiek.tsx
@@ -726,7 +726,7 @@ export function PrijzenGrafiek({
 
         {/* Graph */}
         {chartData && chartData.length > 0 ? (
-          <div className="h-96 w-full relative" style={{ minWidth: 0, minHeight: 384 }}>
+          <div className="h-96 w-full relative" style={{ minWidth: '1px', minHeight: '384px', width: '100%', height: '384px' }}>
             <ResponsiveContainer width="100%" height="100%" minHeight={384} minWidth={0}>
             <BarChart
               data={chartData}
