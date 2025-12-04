e98c70beefd59e1ef5a8822ab91f9962c0f9bec7
Rick Schlimback <rickschlimback@iMac-van-Rick.local>
2025-12-04 14:52:53 +0100

    Fix: Facebook Pixel component verbeterd met error handling en veiligere implementatie

--- src/components/tracking/FacebookPixel.tsx
+++ src/components/tracking/FacebookPixel.tsx
@@ -25,11 +25,23 @@ export function FacebookPixel({ pixelId }: FacebookPixelProps) {
   useEffect(() => {
     // Track page view on route change
     if (typeof window !== 'undefined' && window.fbq) {
-      window.fbq('track', 'PageView')
+      try {
+        window.fbq('track', 'PageView')
+      } catch (error) {
+        console.error('Facebook Pixel tracking error:', error)
+      }
     }
   }, [pathname])
 
-  if (!pixelId) {
+  if (!pixelId || !pixelId.trim()) {
+    return null
+  }
+
+  // Escape pixelId to prevent XSS
+  const safePixelId = pixelId.replace(/[^0-9]/g, '')
+
+  if (!safePixelId) {
+    console.error('Invalid Facebook Pixel ID:', pixelId)
     return null
   }
 
@@ -38,18 +50,29 @@ export function FacebookPixel({ pixelId }: FacebookPixelProps) {
       <Script
         id="facebook-pixel"
         strategy="afterInteractive"
+        onError={(e) => {
+          console.error('Facebook Pixel script error:', e)
+        }}
         dangerouslySetInnerHTML={{
           __html: `
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '${pixelId}');
-            fbq('track', 'PageView');
+            !function(f,b,e,v,n,t,s){
+              if(f.fbq)return;
+              n=f.fbq=function(){
+                n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)
+              };
+              if(!f._fbq)f._fbq=n;
+              n.push=n;
+              n.loaded=!0;
+              n.version='2.0';
+              n.queue=[];
+              t=b.createElement(e);
+              t.async=!0;
+              t.src=v;
+              s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)
+            }(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init','${safePixelId}');
+            fbq('track','PageView');
           `,
         }}
       />
@@ -58,7 +81,7 @@ export function FacebookPixel({ pixelId }: FacebookPixelProps) {
           height="1"
           width="1"
           style={{ display: 'none' }}
-          src={`https://www.facebook.com/tr?id=${pixelId}&ev=PageView&noscript=1`}
+          src={`https://www.facebook.com/tr?id=${safePixelId}&ev=PageView&noscript=1`}
           alt=""
         />
       </noscript>
